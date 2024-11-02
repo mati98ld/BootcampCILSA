e966d407153498eae63bdf96bb1fd3bf2fd0ac83
Matías Díaz <mati98ld@gmail.com>
2024-11-02 16:51:26 -0300

    Se agregó sección de contacto y se mejoro estructura

--- js/main.js
+++ js/main.js
@@ -13,21 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => console.error("Error al cargar el navbar: ", error));
 });
 
-// Renderiza el navbar para la sección de productos
-document.addEventListener("DOMContentLoaded", function () {
-  // Selecciono el div donde se debe cargar el navbar
-  let navbar = document.getElementById("navbar-prod");
-
-  // Solicitud AJAX
-  fetch("/partials/navbar-prod.html")
-    .then((response) => response.text()) // Convierto la respuesta a texto
-    .then((data) => {
-      // Cargo el contenido en el div
-      navbar.innerHTML = data;
-    })
-    .catch((error) => console.error("Error al cargar el navbar: ", error));
-});
-
 // Renderiza el footer
 document.addEventListener("DOMContentLoaded", function () {
   // Selecciono el div donde se debe cargar el navbar
